2bf14ba125862518913484b4082c8a7de16955df
Sergio Pizzolante <sergio.pizzolante95@gmail.com>
2024-04-17 22:03:40 +0200

    working streamlit app

--- project_folder/streamlit_app.py
+++ project_folder/streamlit_app.py
@@ -1,20 +1,10 @@
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestRegressor
 import streamlit as st
 import pandas as pd
 import pickle
 
-# Load model and preprocessing objects
-with open('model.pkl', 'rb') as file:
-    scaler, ct, pipeline = pickle.load(file)
-
-def predict_salary(input_data):
-    # Make predictions
-    predictions = pipeline.predict(input_data)
-    return predictions
+# Load model
+with open('pipeline_model.pkl', 'rb') as file:
+    pipeline = pickle.load(file)
 
 # Streamlit UI
 st.title('Salary Prediction App')
@@ -36,26 +26,14 @@ input_data = pd.DataFrame({
     'employee_residence': [employee_residence],
     'remote_ratio': [remote_ratio],
     'company_location': [company_location],
-    'company_size': [company_size],
-    'salary_in_usd': [0]  # Placeholder value
+    'company_size': [company_size]
 })
 
 # Predict salary
 if st.button('Predict Salary'):
-    # Transform input data
-    input_data_transformed = ct.transform(input_data)
-
-    original_categorical_columns = ['work_year', 'experience_level', 'job_title', 'employee_residence', 'remote_ratio', 'company_location', 'company_size']
-    original_categories = {
-    'work_year': [2020, 2021, 2022, 2023],
-    'experience_level': ['SE', 'MI', 'EN', 'EX'],
-    'job_title': ['Data Science & Machine Learning', 'Data Engineering & Infrastructure', 'Data Analysis & Analytics', 'Data Management', 'Artificial Intelligence (AI)', 'Business Intelligence (BI)'],
-    'employee_residence': ['North America', 'Europe', 'Other'],
-    'remote_ratio': [0, 50, 100],
-    'company_location': ['North America', 'Europe', 'Other'],
-    'company_size': ['S', 'M', 'L']
-}
-  
     # Predict salary
-    prediction = pipeline.predict(input_data_transformed)
-    st.success(f'Predicted Salary: {prediction[0]}')
+    prediction = pipeline.predict(input_data)
+    
+    formatted_salary = '${:.2f}'.format(prediction[0])
+    
+    st.success(f'Predicted Salary: {formatted_salary}') 
